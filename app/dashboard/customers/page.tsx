6eba189eee44feeffc82f30810eb3435fca548da
jiayaooli <jiayaooli@163.com>
2024-08-25 22:47:09 +0800

    feat:fetch date

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,28 @@
+'use client'
 export default function Page() {
     return <p>
-        customer page
+        customer page {mainProcess()}
     </p>
+
 }
+
+function mainProcess() {
+    let promise = new Promise(function (resolve, reject){
+        setTimeout(() => reject("success"), 1000)
+    })
+    promise.then((message) => {
+        console.log(`success ${message}`)
+    })
+    promise.then((message) => {
+        console.log(`success ${message}`)
+    }, (message) => {
+        console.log(`fail${message}`)
+    })
+    console.log("hh")
+}
+
+
+
+
+
+
